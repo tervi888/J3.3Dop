3b377b81e2b9df14f15e25efa9ba458bddd2ed18
Tervi test <Tervi@list.ru>
2023-04-18 21:46:58 +0300

    correction month

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,9 @@
 public class CreditPaymentService {
     public double CreditPaymentServicecalculate(int amount, int month, double bet) {
-        double bet1 = (bet * 0.01) / month;
+        double bet1 = (bet * 0.01) / 12;
+        System.out.println(bet1);
         double bet3 = Math.pow((1 + bet1), month);
+        System.out.println(bet3);
         double payment = ((bet1 * amount) * bet3) / (bet3 - 1);
 
         return payment;
